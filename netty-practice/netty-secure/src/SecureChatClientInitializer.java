37afb7fed1c246581bb0b9892979616c4063125f
Michelle Duer <michelle.duer@gmail.com>
2017-04-26 13:05:06 -0700

    implement a secure chat server, success connecting to port

---
+++ netty-practice/netty-secure/src/SecureChatClientInitializer.java
@@ -0,0 +1,36 @@
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.ssl.SslContext;
+
+
+/**
+ * Created by michelle on 4/26/17.
+ */
+
+// Creates a newly configured pipeline for a new channel
+public class SecureChatClientInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SslContext sslCtx;
+
+    public SecureChatClientInitializer(SslContext sslCtx) {
+        this.sslCtx = sslCtx;
+    }
+
+    @Override
+    public void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        // This example uses a bogus certificate TODO: needs editing or removal?
+        pipeline.addLast(sslCtx.newHandler(ch.alloc(), SecureChatClient.HOST, SecureChatClient.PORT));
+
+        pipeline.addLast(new DelimiterBasedFrameDecoder(8192, Delimiters.lineDelimiter()));
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
+        pipeline.addLast(new SecureChatServerHandler());
+    }
+}
